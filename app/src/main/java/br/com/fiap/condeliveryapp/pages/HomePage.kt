b3dffb7b384448a6c4951b811fa815eba77acf4b
Jeferson nery <95384459+JefersonNery@users.noreply.github.com>
2024-09-29 10:47:42 -0300

    CondeliveryApp

--- app/src/main/java/br/com/fiap/condeliveryapp/pages/HomePage.kt
+++ app/src/main/java/br/com/fiap/condeliveryapp/pages/HomePage.kt
@@ -1,42 +1,200 @@
 package br.com.fiap.condeliveryapp.pages
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import br.com.fiap.condeliveryapp.AuthState
 import br.com.fiap.condeliveryapp.AuthViewModel
+import br.com.fiap.condeliveryapp.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomePage(modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel) {
+fun HomePage(
+    modifier: Modifier = Modifier, navController: NavController, authViewModel: AuthViewModel
+) {
 
     val authState = authViewModel.authState.observeAsState()
 
+    var codigo by remember {
+        mutableStateOf("")
+    }
+
+    var textState = remember { mutableStateOf(TextFieldValue()) }
+
     LaunchedEffect(authState.value) {
-        when (authState.value){
+        when (authState.value) {
             is AuthState.Unauthenticated -> navController.navigate("login")
             else -> Unit
         }
     }
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Home Page", fontSize = 32.sp)
+        Image(
+            painter = painterResource(id = R.drawable.headerimg),
+            contentDescription = "Header imagem",
+            modifier = Modifier.height(150.dp)
+        )
+        Column(
+            modifier = Modifier.fillMaxWidth().offset(y = (-40.dp)), horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Alameda Mamoré, nº 809 - apt 513 - 6º andar",
+                Modifier
+                    .fillMaxWidth()
+                    .background(color = colorResource(id = R.color.bglivery))
+                    .padding(10.dp)
+            )
+        }
+        Column(
+            modifier = Modifier.offset(y = (-75.dp)),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Bem Vindo(a) de volta !",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(50.dp),
+                fontSize = 30.sp,
+                color = colorResource(id = R.color.txtlivery)
+            )
+
+            Text("Fez o pedido no app parceiro ?", fontSize = 22.sp)
+            Text(
+                "Que maravilha!! nos informe qual o app parceiro, o código e deixa que cuidamos do resto.",
+                Modifier
+                    .fillMaxWidth()
+                    .padding(30.dp),
+                textAlign = TextAlign.Center
+
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.parceiros),
+                contentDescription = "Header imagem",
+                modifier = Modifier.height(80.dp)
+            )
+
+            Column(
+                modifier = Modifier
+                    .width(400.dp)
+                    .height(150.dp)
+                    .background(color = colorResource(id = R.color.bglivery))
+                    .clip(RoundedCornerShape(70.dp)),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                TextField(
+                    value = codigo,
+                    onValueChange = { codigo = it },
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(300.dp)
+                        .background(color = colorResource(id = R.color.txtlivery))
+                        .clip(RoundedCornerShape(10.dp)),
+                    label = {
+                        Text(text = "Digite o Codigo da entrega")
+                    },
+                    shape = RoundedCornerShape(4.dp),
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Button(
+                    onClick = {
+                        println("Obrigado, Logo levarei seu pedido.")
+                    },
+                    modifier = Modifier
+                        .width(150.dp)
+                        .height(45.dp)
+                        .background(color = colorResource(id = R.color.bglivery))
+                ) {
+                    Text("Enviar")
+                }
+            }
+        }
+
+        Text(
+            "Se necessário, você pode insirir instruções de entrega para nosso entregador abaixo :",
+            fontSize = 15.sp,
+            modifier = Modifier
+                .width(400.dp)
+                .offset(y = (-50.dp)),
+            textAlign = TextAlign.Center
+
+        )
+
+        OutlinedTextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            placeholder = { Text("EX: Deixe na porta, garagem etc.") },
+            modifier = Modifier.width(400.dp).height(130.dp).offset(y = (-40.dp)),
+            shape = RoundedCornerShape(8.dp),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Gray, unfocusedBorderColor = Color.LightGray
+            )
+        )
 
         TextButton(onClick = {
             authViewModel.signout()
         }) {
             Text(text = "Deslogar")
         }
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = 40.dp),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.menu),
+            contentDescription = "Menu imagem",
+            modifier = Modifier.height(80.dp)
+        )
     }
 }
--- app/src/main/java/br/com/fiap/condeliveryapp/pages/LoginPage.kt
+++ app/src/main/java/br/com/fiap/condeliveryapp/pages/LoginPage.kt
@@ -3,16 +3,21 @@ package br.com.fiap.condeliveryapp.pages
 import android.provider.CalendarContract.Colors
 import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -37,13 +42,18 @@ import br.com.fiap.condeliveryapp.AuthState
 import br.com.fiap.condeliveryapp.AuthViewModel
 import br.com.fiap.condeliveryapp.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginPage(modifier: Modifier = Modifier,navController: NavController, authViewModel: AuthViewModel) {
+fun LoginPage(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    authViewModel: AuthViewModel
+) {
 
     var email by remember {
         mutableStateOf("")
     }
-    
+
     var password by remember {
         mutableStateOf("")
     }
@@ -52,10 +62,13 @@ fun LoginPage(modifier: Modifier = Modifier,navController: NavController, authVi
     val context = LocalContext.current
 
     LaunchedEffect(authState.value) {
-        when(authState.value){
+        when (authState.value) {
             is AuthState.Authenticated -> navController.navigate("home")
-            is AuthState.Error -> Toast.makeText(context,
-                (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT).show()
+            is AuthState.Error -> Toast.makeText(
+                context,
+                (authState.value as AuthState.Error).message, Toast.LENGTH_SHORT
+            ).show()
+
             else -> Unit
         }
     }
@@ -65,50 +78,74 @@ fun LoginPage(modifier: Modifier = Modifier,navController: NavController, authVi
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.imglogin),
-            contentDescription = "Descrição da imagem",
-            modifier = Modifier.height(200.dp).offset(y = (-140).dp)
-
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        OutlinedTextField(
-            value = email,
-            onValueChange = {email = it},
-            label = { Text("Ex: Fulano@gmail.com") },
-            shape = RoundedCornerShape(16.dp),
-
-        )
-        
-        Spacer(modifier = Modifier.height(8.dp))
-
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = {
-                Text(text = "Senha")
-            },
-            shape = RoundedCornerShape(16.dp),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(onClick = {
-            authViewModel.login(email, password)
-        },
-            enabled = authState.value != AuthState.Loading) {
-            Text(text = "Entrar")
+        Column() {
+            Image(
+                painter = painterResource(id = R.drawable.headerimg),
+                contentDescription = "Header imagem",
+                modifier = Modifier
+                    .height(150.dp)
+                    .offset(y = (-180).dp)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.imglogin),
+                contentDescription = "Login Imagem",
+                modifier = Modifier
+                    .height(200.dp)
+                    .offset(y = (-160).dp)
+            )
         }
-        
-         Spacer(modifier = Modifier.height(8.dp))
 
-        TextButton(onClick = {
-            navController.navigate("cadastrar")
-        }) {
-            Text(text = "Cadastrar uma conta nova")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(
+            modifier = Modifier.offset(y = (-120).dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Ex: Fulano@gmail.com") },
+                shape = RoundedCornerShape(16.dp),
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = {
+                    Text(text = "Senha")
+                },
+                shape = RoundedCornerShape(16.dp),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = (-25.dp)),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextButton(onClick = {
+                    navController.navigate("cadastrar")
+                }) {
+                    Text(text = "Cadastrar uma conta nova")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+                    onClick = {
+                        authViewModel.login(email, password)
+                    },
+                    enabled = authState.value != AuthState.Loading
+                ) {
+                    Text(text = "Entrar")
+                }
+            }
+
         }
     }
 }
